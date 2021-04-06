66ca8410acbecd2287ab19ef08660921adc97d75
Marlon Hanks <owner@kedia.co>
2021-04-06 01:24:12 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 // Copyright 2019-2020 Parity Technologies (UK) Ltd.
-// This file is part of Substrate.
+// This file is part of Tetcore.
 
-// Substrate is free software: you can redistribute it and/or modify
+// Tetcore is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Substrate is distributed in the hope that it will be useful,
+// Tetcore is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
+// along with Tetcore.  If not, see <http://www.gnu.org/licenses/>.
 
 //! # WASM builder runner
 //!
@@ -20,8 +20,8 @@
 //! resolution, we need this little tool. See <https://github.com/rust-lang/cargo/issues/5730> for
 //! more information.
 //!
-//! It will create a project that will call `substrate-wasm-builder` to prevent any dependencies
-//! from `substrate-wasm-builder` influencing the main project's dependencies.
+//! It will create a project that will call `wasm-builder` to prevent any dependencies
+//! from `wasm-builder` influencing the main project's dependencies.
 //!
 //! For more information see <https://crates.io/substrate-wasm-builder>
 
